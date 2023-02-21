a22b9b5cf5bdb9602b0b98c447620de7c5d7bc6e
Fan Sword <fanmeijian@gmail.com>
2023-02-21 17:31:18 +0800

    for model permission

--- src/main/java/cn/sparrowmini/org/pem/server/CurrentUserFilter.java
+++ src/main/java/cn/sparrowmini/org/pem/server/CurrentUserFilter.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import cn.sparrowmini.pem.model.common.CurrentUser;
+import cn.sparrowmini.common.CurrentUser;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
