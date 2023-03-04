f4e23f24528051f61616a25ff7fa4ae86cabcfa4
Fan Sword <fanmeijian@gmail.com>
2023-03-04 10:36:29 +0800

    完善群组服务

--- src/main/java/cn/sparrowmini/org/pem/server/OpenApiConfig.java
+++ src/main/java/cn/sparrowmini/org/pem/server/OpenApiConfig.java
@@ -3,8 +3,12 @@ package cn.sparrowmini.org.pem.server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 
 @Configuration
 public class OpenApiConfig {
@@ -15,14 +19,14 @@ public class OpenApiConfig {
 	 */
 	@Bean
 	public OpenAPI springShopOpenAPI() {
-//		SecurityScheme securityScheme = new SecurityScheme();
-//		securityScheme.setScheme("bearer");
-//		securityScheme.setType(Type.HTTP);
-//		securityScheme.setBearerFormat("JWT");
+		SecurityScheme securityScheme = new SecurityScheme();
+		securityScheme.setScheme("bearer");
+		securityScheme.setType(Type.HTTP);
+		securityScheme.setBearerFormat("JWT");
 		
 		return new OpenAPI()
-//				.components(new Components().addSecuritySchemes("bearerAuth",securityScheme ))
-//				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+				.components(new Components().addSecuritySchemes("bearerAuth",securityScheme ))
+				.addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
 				.info(new Info().title("Sparrow Org Service")
 				.description("组织管理").version("v0.0.1"));
 	}
