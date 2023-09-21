68a4b68cef4966cc66ad7ca16fb153fcdf1ffffc
Fan Sword <fanmeijian@gmail.com>
2023-09-21 19:27:46 +0800

    增加门户的包扫描

--- src/main/java/cn/sparrowmini/org/pem/server/SparrowOrgPemApplication.java
+++ src/main/java/cn/sparrowmini/org/pem/server/SparrowOrgPemApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EntityScan({"cn.sparrowmini.org","cn.sparrowmini.pem","cn.sparrowmini.common"})
+@EntityScan({"cn.sparrowmini.org","cn.sparrowmini.pem","cn.sparrowmini.common","cn.sparrowmini.portal"})
 @ComponentScan("cn.sparrowmini")
 @EnableJpaRepositories("cn.sparrowmini")
 @SpringBootApplication
